872626d5cd3c1bbd3291ec5426cc0fa4d4fc24ac
Usuario <Usuario@lab-105-desktop-1>
2023-09-26 12:20:11 +0200

    Primer commit

---
+++ src/main/java/Aplicacion/PracticaInicial.java
@@ -0,0 +1,16 @@
+package Aplicacion;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class PracticaInicial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
